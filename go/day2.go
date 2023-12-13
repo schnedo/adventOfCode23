5802e933072e8feb90b8974a30bae911feb6e6ea
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2023-12-13 17:28:56 +0100

    day2,go: solve part two

--- go/day2.go
+++ go/day2.go
@@ -22,6 +22,10 @@ func (c *colorset) isMoreThan(other *colorset) bool {
 	return c.blue > other.blue || c.green > other.green || c.red > other.red
 }
 
+func (c *colorset) power() int {
+	return c.blue * c.red * c.green
+}
+
 type game struct {
 	id   int
 	sets []colorset
@@ -45,6 +49,30 @@ func (g *game) isPossibleWith(bag *colorset) bool {
 		}
 	}
 	return true
+}
+
+func enclosingSet(sets []colorset) *colorset {
+	minBlue := 0
+	minRed := 0
+	minGreen := 0
+
+	for _, set := range sets {
+		if set.blue > minBlue {
+			minBlue = set.blue
+		}
+		if set.red > minRed {
+			minRed = set.red
+		}
+		if set.green > minGreen {
+			minGreen = set.green
+		}
+	}
+
+	return &colorset{
+		green: minGreen,
+		red:   minRed,
+		blue:  minBlue,
+	}
 }
 
 func parseColorset(line string) colorset {
@@ -90,16 +118,19 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	bag := &colorset{red: 12, green: 13, blue: 14}
+	sumOfPossibles := 0
+	sumOfEnclosingPowers := 0
 
-	sum := 0
 	for scanner.Scan() {
 		game := parseGame(scanner.Text())
 		if game.isPossibleWith(bag) {
-			sum += game.id
+			sumOfPossibles += game.id
 		}
+		sumOfEnclosingPowers += enclosingSet(game.sets).power()
 	}
 
 	file.Close()
 
-	fmt.Println(sum)
+	fmt.Println("sumOfPossibles: " + strconv.Itoa(sumOfPossibles))
+	fmt.Println("sumOfEnclosingPowers: " + strconv.Itoa(sumOfEnclosingPowers))
 }
