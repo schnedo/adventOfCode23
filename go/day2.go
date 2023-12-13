3f98f442af0577237ab730aa252c5527012d4f02
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2023-12-13 16:46:29 +0100

    day2,go: parse lines

--- go/day2.go
+++ go/day2.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type colorset struct {
@@ -12,23 +14,60 @@ type colorset struct {
 	green int
 }
 
+func (c *colorset) String() string {
+	return "{red: " + strconv.Itoa(c.red) + ", blue: " + strconv.Itoa(c.blue) + ", green: " + strconv.Itoa(c.green) + "}"
+}
+
 type game struct {
 	id   int
 	sets []colorset
 }
 
-func parseColorset(line string) *colorset {
-	return &colorset{
-		blue:  0,
-		red:   0,
-		green: 0,
+func (g *game) String() string {
+	colorsets := ""
+	for i, set := range g.sets {
+		colorsets = colorsets + set.String()
+		if i != len(g.sets)-1 {
+			colorsets = colorsets + " "
+		}
+	}
+	return "{" + strconv.Itoa(g.id) + " [" + colorsets + "]}"
+}
+
+func parseColorset(line string) colorset {
+	lineSplit := strings.Split(line, ",")
+
+	colorMap := map[string]int{}
+
+	for _, setString := range lineSplit {
+		colorsetSplit := strings.Split(setString, " ")
+		nColor, _ := strconv.Atoi(colorsetSplit[len(colorsetSplit)-2])
+		colorMap[colorsetSplit[len(colorsetSplit)-1]] = nColor
+	}
+
+	return colorset{
+		blue:  colorMap["blue"],
+		red:   colorMap["red"],
+		green: colorMap["green"],
 	}
 }
 
 func parseGame(line string) *game {
+	lineSplit := strings.Split(line, ":")
+	gameSplit := strings.Split(lineSplit[0], " ")
+
+	gameId, _ := strconv.Atoi(gameSplit[1])
+
+	colorsetSplit := strings.Split(lineSplit[1], ";")
+
+	sets := []colorset{}
+	for _, line := range colorsetSplit {
+		sets = append(sets, parseColorset(line))
+	}
+
 	return &game{
-		id:   0,
-		sets: []colorset{},
+		id:   gameId,
+		sets: sets,
 	}
 }
 
