abcd08fd19e1627b38200cd2c9af234ef9a8e435
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2023-12-14 20:04:37 +0100

    day3,go: solve part one

--- go/day3/day3.go
+++ go/day3/day3.go
@@ -9,10 +9,59 @@ import (
 
 func main() {
 
+	var lastLine *schematicLine
+	var notYetAdjacents []number
+	sum := 0
 	for rawLine := range internal.ReadLines("day3") {
+		fmt.Println("----------------")
+		fmt.Println("raw:")
+		fmt.Println(rawLine)
 		line := parseLine(rawLine)
-		fmt.Println(line)
+		adjacents, notAdjacents := checkAdjacent(line.numbers, line.symbols)
+		fmt.Println("adjacents:")
+		for _, num := range adjacents {
+			fmt.Print(strconv.Itoa(num.value) + ",")
+			sum += num.value
+		}
+		adjacents, _ = checkAdjacent(notYetAdjacents, line.symbols)
+		for _, num := range adjacents {
+			fmt.Print(strconv.Itoa(num.value) + ",")
+			sum += num.value
+		}
+		notYetAdjacents = notAdjacents
+		if lastLine != nil {
+			adjacents, _ = checkAdjacent(line.numbers, lastLine.symbols)
+		}
+		for _, num := range adjacents {
+			fmt.Print(strconv.Itoa(num.value) + ",")
+			sum += num.value
+		}
+		lastLine = &line
+		fmt.Println()
 	}
+	fmt.Println(sum)
+}
+
+func checkAdjacent(numbers []number, symbols []symbol) (adjacents, notAdjacents []number) {
+	symbolsStartIndex := 0
+numLoop:
+	for _, num := range numbers {
+		for i := symbolsStartIndex; i < len(symbols); i++ {
+			if num.positionRangeStart > symbols[i].position {
+				symbolsStartIndex = i + 1
+			}
+			if areAdjacent(num, symbols[i]) {
+				adjacents = append(adjacents, num)
+				continue numLoop
+			}
+		}
+		notAdjacents = append(notAdjacents, num)
+	}
+	return
+}
+
+func areAdjacent(num number, sym symbol) bool {
+	return sym.position >= num.positionRangeStart-1 && sym.position <= num.positionRangeEnd
 }
 
 var numberOrSymbolPattern = regexp.MustCompile("\\d+|[^.]")
