3ac66976861d2fba0ebd51eb35a90105217d961c
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2023-12-15 13:42:56 +0100

    day3,go: solve part two

--- go/day3/day3.go
+++ go/day3/day3.go
@@ -12,34 +12,48 @@ func main() {
 	var lastLine *schematicLine
 	var notYetAdjacents []number
 	sum := 0
+	gears := []symbol{}
+
 	for rawLine := range internal.ReadLines("day3") {
-		fmt.Println("----------------")
-		fmt.Println("raw:")
-		fmt.Println(rawLine)
 		line := parseLine(rawLine)
 		adjacents, notAdjacents := checkAdjacent(line.numbers, line.symbols)
-		fmt.Println("adjacents:")
 		for _, num := range adjacents {
-			fmt.Print(strconv.Itoa(num.value) + ",")
 			sum += num.value
 		}
 		adjacents, _ = checkAdjacent(notYetAdjacents, line.symbols)
 		for _, num := range adjacents {
-			fmt.Print(strconv.Itoa(num.value) + ",")
 			sum += num.value
 		}
 		notYetAdjacents = notAdjacents
 		if lastLine != nil {
 			adjacents, _ = checkAdjacent(line.numbers, lastLine.symbols)
+			gears = append(gears, filterGears(lastLine.symbols)...)
 		}
 		for _, num := range adjacents {
-			fmt.Print(strconv.Itoa(num.value) + ",")
 			sum += num.value
 		}
 		lastLine = &line
-		fmt.Println()
 	}
 	fmt.Println(sum)
+	gears = append(gears, filterGears(lastLine.symbols)...)
+
+	sumOfGearRatios := 0
+
+	for _, gear := range gears {
+		sumOfGearRatios += gear.adjacentNums[0] * gear.adjacentNums[1]
+	}
+
+	fmt.Println(sumOfGearRatios)
+}
+
+func filterGears(symbols []symbol) []symbol {
+	gears := []symbol{}
+	for _, sym := range symbols {
+		if sym.value == "*" && len(sym.adjacentNums) == 2 {
+			gears = append(gears, sym)
+		}
+	}
+	return gears
 }
 
 func checkAdjacent(numbers []number, symbols []symbol) (adjacents, notAdjacents []number) {
@@ -52,6 +66,7 @@ numLoop:
 			}
 			if areAdjacent(num, symbols[i]) {
 				adjacents = append(adjacents, num)
+				symbols[i].adjacentNums = append(symbols[i].adjacentNums, num.value)
 				continue numLoop
 			}
 		}
@@ -78,7 +93,9 @@ type number struct {
 }
 
 type symbol struct {
-	position int
+	value        string
+	position     int
+	adjacentNums []int
 }
 
 func parseLine(line string) schematicLine {
@@ -86,11 +103,12 @@ func parseLine(line string) schematicLine {
 
 	numbers, symbols := []number{}, []symbol{}
 	for _, matchedRange := range matchedRanges {
-		num, err := strconv.Atoi(line[matchedRange[0]:matchedRange[1]])
+		value := line[matchedRange[0]:matchedRange[1]]
+		num, err := strconv.Atoi(value)
 		if err == nil {
 			numbers = append(numbers, number{value: num, positionRangeStart: matchedRange[0], positionRangeEnd: matchedRange[1]})
 		} else {
-			symbols = append(symbols, symbol{position: matchedRange[0]})
+			symbols = append(symbols, symbol{position: matchedRange[0], value: value})
 		}
 	}
 
