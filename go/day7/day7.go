735a964ad601b0fd2dfe958a974ea7c0bb705cab
Dominik Schneider (schnedo) <schnedo@users.noreply.github.com>
2023-12-28 16:00:03 +0100

    day7,go: solve part one

--- go/day7/day7.go
+++ go/day7/day7.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"myadvent/internal"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -14,13 +15,20 @@ func main() {
 		hands = append(hands, hand)
 	}
 
-	fmt.Println(hands)
+	slices.SortFunc(hands, compareHands)
+
+	totalWinnings := 0
+	for i, hand := range hands {
+		totalWinnings += (i + 1) * hand.bid
+	}
+
+	fmt.Println(totalWinnings)
 }
 
-type Card int8
+type Card int16
 
 const (
-	two Card = 2 + iota
+	two Card = iota
 	three
 	four
 	five
@@ -35,9 +43,75 @@ const (
 	ace
 )
 
+type Type int8
+
+const (
+	highCard Type = iota
+	onePair
+	twoPair
+	threeOfAKind
+	fullHouse
+	fourOfAKind
+	fiveOfAKind
+)
+
 type Hand struct {
 	cards [5]Card
 	bid   int
+	ttype Type
+}
+
+func compareHands(a, b Hand) int {
+	if a.ttype < b.ttype {
+		return -1
+	}
+	if a.ttype > b.ttype {
+		return 1
+	}
+	for i, aCard := range a.cards {
+		bCard := b.cards[i]
+		if aCard < bCard {
+			return -1
+		}
+		if aCard > bCard {
+			return 1
+		}
+	}
+	panic("Is same hand even possible?")
+}
+
+func getTypeOf(cards [5]Card) Type {
+	counts := make(map[Card]int8)
+	for _, card := range cards {
+		counts[card]++
+	}
+	switch len(counts) {
+	case 1:
+		return fiveOfAKind
+	case 5:
+		return highCard
+	case 4:
+		return onePair
+	case 3:
+		for _, count := range counts {
+			if count == 3 {
+				return threeOfAKind
+			}
+			if count == 2 {
+				return twoPair
+			}
+		}
+		panic("type of hand with 3 different cards seems fishy")
+	case 2:
+		for _, count := range counts {
+			if count == 4 || count == 1 {
+				return fourOfAKind
+			} else {
+				return fullHouse
+			}
+		}
+	}
+	panic("impossible count of cards received")
 }
 
 func parseCard(label string) Card {
@@ -85,5 +159,6 @@ func parseHand(line string) Hand {
 	return Hand{
 		cards: cards,
 		bid:   bid,
+		ttype: getTypeOf(cards),
 	}
 }
